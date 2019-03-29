7f5ecfa8cdd514cffe1f21c9da7cc2635fd1a380
Justin Schlankey <justin.schlankey@yahoo.com>
2019-03-29 16:22:16 -0500

    updated

--- Solved/logic.js
+++ Solved/logic.js
@@ -12,7 +12,7 @@ function createFeatures(earthquakeData) {
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
+    layer.bindPopup("<h3>" + feature.properties.mag + "</h3><h3>" + feature.properties.place + 
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
 
